8059e2edc646de190a6af2ad72ccd5a59b241c01
Lousie <61540072+Kotora-source@users.noreply.github.com>
2021-04-27 15:24:01 +0200

    Update ball.lua

--- Projet casse briques/src/models/ball.lua
+++ Projet casse briques/src/models/ball.lua
@@ -10,11 +10,11 @@ Ball = {
   speedY = 0,
   scalewidth = 0,
   scaleheight = 0,
-  stats = "aucune stat particulière, c'est la balle de base"
+  impact = 0
 }
 
 -----------------------------------------------------------------------------------------
---Constructeur
+--Constructeur de base
   --Mettre nil pdt la creation de l'objet
 function Ball:new(o)
   o = o or {}
@@ -29,9 +29,16 @@ function Ball:new(o)
   self.y = 0
   self.speedX = 0
   self.speedY = 0
+  self.impact = 0
   return o
 end
 
+--[[faire un constructeur de balles avec d'autres spécificités
+genre faire un tableau pour pouvoir stocker les noms de PATH dynamiquement et pas se 
+casser le cul à faire un if else chiant là
+et créer des balles avec des effets ptet, des skins difs, une force d'impact plus mieux que celle
+de base, des balles qui se divisent en plusieurs jsp fais travailler ton imagination
+]]--
 -----------------------------------------------------------------------------------------
 --GETTERS
 function Ball:getName()
